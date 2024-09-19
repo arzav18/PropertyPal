9f1d79e8ec23656df6dbccc1adb9f63c14193db4
Arzav Jain <arzav.jain@gmail.com>
2024-09-19 17:38:56 +0530

    added map, filter, singlePage, and listPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import Navbar from "./components/Navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./pages/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./pages/ListPage/ListPage";
+import Layout from "./pages/Layout/Layout";
+import SinglePage from "./pages/SinglePage/SinglePage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./slider.scss";
+
+function Slider() {
+  return <div className="slider">slider</div>;
+}
+import "./slider.scss";
+
+export default Slider;
