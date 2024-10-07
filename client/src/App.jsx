52b88781ca8ded07ec1f949cea91252da53d996d
Arzav Jain <arzav.jain@gmail.com>
2024-10-07 17:11:48 +0530

    Fetch Post data and display it on UI using react-router-dom loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/login/Login";
 import { Layout, RequireAuth } from "../src/pages/Layout/Layout";
 import ProfileUpdatePage from "./pages/ProfileUpdatePage/ProfileUpdatePage";
 import NewPostPage from "./pages/NewPostPage/NewPostPage";
+import { singlePageLoader } from "./lib/loaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -26,6 +27,7 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
+          loader: singlePageLoader,
         },
 
         {
--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
 import "./map.scss";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import Pin from '../Pin/Pin';
+import Pin from "../Pin/Pin";
 
-function Map({items}) {
+function Map({ items }) {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className='map'>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {items.map(item=>(
+    <MapContainer
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [20.5937, 78.9629]
+      }
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
         <Pin item={item} key={item.id} />
-    ))}
-  </MapContainer>
-  )
+      ))}
+    </MapContainer>
+  );
 }
 
-export default Map
+export default Map;
